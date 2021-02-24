5c9402896260d078e826cce9328fcab1fc39db5b
Amchayd Mariam <meriam.amchayd@gmail.com>
2021-02-24 16:01:23 +0100

    Initial commit

---
+++ src/main/java/org/enset/gestionprojet/model/Project.java
@@ -0,0 +1,8 @@
+package org.enset.gestionprojet.model;
+
+public class Project {
+
+    private Long id;
+    private String titre;
+    private String description;
+}
---
+++ src/main/java/org/enset/gestionprojet/model/Responsable.java
@@ -0,0 +1,4 @@
+package org.enset.gestionprojet.model;
+
+public class Responsable {
+}
